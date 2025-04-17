48d6605b9bb2d14147cc5e39dd466fc186158515
JhonForero44 <jhonforero44@gmail.com>
2025-04-17 18:10:51 -0500

    Actualizacion de contraseña

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -2,16 +2,15 @@
 const pool = require('../config/db');
 const bcrypt = require('bcrypt');
 
-// Función para registrar un usuario
 const createUser = async (cedula, nombre_usuario, email, password) => {
   const hashedPassword = await bcrypt.hash(password, 10); // Encriptamos la contraseña
   const query = `
-    INSERT INTO usuarios (cedula, nombre_usuario, email, password_hash) 
-    VALUES ($1, $2, $3, $4) 
-    RETURNING usuario_id, cedula, nombre_usuario, email, foto_usuario, fecha_registro, saldo
+    INSERT INTO usuarios (cedula, nombre_usuario, email, password_hash, estado_cuenta) 
+    VALUES ($1, $2, $3, $4, 'pendiente') 
+    RETURNING usuario_id, cedula, nombre_usuario, email, foto_usuario, fecha_registro, saldo, estado_cuenta
   `;
   const values = [cedula, nombre_usuario, email, hashedPassword];
-  
+
   try {
     const result = await pool.query(query, values);
     return result.rows[0]; // Retornamos el usuario registrado
@@ -24,7 +23,7 @@ const createUser = async (cedula, nombre_usuario, email, password) => {
 const getUserByEmail = async (email) => {
   const query = 'SELECT * FROM usuarios WHERE email = $1';
   const values = [email];
-  
+
   try {
     const result = await pool.query(query, values);
     return result.rows[0]; // Retornamos el primer usuario encontrado
@@ -41,4 +40,13 @@ const aumentarSaldoUsuario = async (usuario_id, monto) => {
   `, [monto, usuario_id]);
 };
 
-module.exports = { createUser, getUserByEmail, aumentarSaldoUsuario };
+// Actualizar la contraseña
+const updateUserPassword = async (email, password) => {
+  const result = await pool.query(
+    'UPDATE usuarios SET password_hash = $1 WHERE email = $2 RETURNING *',
+    [password, email]
+  );
+  return result.rows[0]; // Regresa el usuario actualizado
+};
+
+module.exports = { createUser, getUserByEmail, aumentarSaldoUsuario, updateUserPassword };
