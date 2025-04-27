6a0db8bfea36af706d571229448b7472a593fa56
Jhon Forero <jhonforero44@gmail.com>
2025-04-27 17:35:32 -0500

    Obtener productos por categoria

--- src/controllers/categoriaController.js
+++ src/controllers/categoriaController.js
@@ -10,6 +10,19 @@ const listarCategorias = async (req, res) => {
   }
 };
 
+// Controlador obtener productos por categorias
+const obtenerProductosPorCategoria = async (req, res) => {
+  const categoria_id = req.params.categoria_id;
+
+  try {
+    const productos = await CategoriaModel.getProductosPorCategoria(categoria_id);
+    res.status(200).json({ productos });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener productos', error: error.message });
+  }
+};
+
 module.exports = {
-  listarCategorias
+  listarCategorias,
+  obtenerProductosPorCategoria
 };
